4ae10da27182c8bed81649bc383fb7998ff8d74f
Beatriz Favini Chicaroni <bia.f.chicaroni@uol.com.br>
2021-11-19 21:44:16 -0300

    Juntando os fronts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
