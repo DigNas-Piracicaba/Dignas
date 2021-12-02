c850f305bb6ea67028ac61f6e3de6abe50d9fe60
Igor Milhomens <igor.milhomens@aluno.ufabc.edu.br>
2021-12-01 23:44:27 -0300

    Inserido rotas na componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioUrbanoComponent } from './formulario-urbano/formulario-urbano.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  
+  {path: 'home', component:HomeComponent},
+  {path: 'landing-page', component:LandingPageComponent},
+  {path: 'formulario', component:FormularioComponent},
+  {path: 'formulario-urbano', component:FormularioUrbanoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
