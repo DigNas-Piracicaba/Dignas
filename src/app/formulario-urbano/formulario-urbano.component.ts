19f982abced6fc8d2a03e5f8df8f182c211efbe6
BEXPERTLANCORP\humberto.marinho <humberto.marinho@bexpert.com.br>
2021-12-20 17:27:09 -0300

    Ajuste do forms.

--- src/app/formulario-urbano/formulario-urbano.component.ts
+++ src/app/formulario-urbano/formulario-urbano.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-urbano.component.css']
 })
 export class FormularioUrbanoComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
+    isShown: boolean = false
+    isShown2: boolean = false
+    isShown3: boolean = false
+    isShown4: boolean = false
+
+    ngOnInit(): void {
+    }
+  
+  
+    toggleShowT1() {
+      this.isShown = true;
+    }
+
+    toggleShowF1() {
+      this.isShown = false;
+    }
+
+    toggleShowT2() {
+      this.isShown2 = true;
+    }
+
+    toggleShowF2() {
+      this.isShown2 = false;
+    }
+
+    toggleShowT3() {
+      this.isShown3 = true;
+    }
+
+    toggleShowF3() {
+      this.isShown3 = false;
+    }
+
+    toggleShowT4() {
+      this.isShown4 = true;
+    }
+
+    toggleShowF4() {
+      this.isShown4 = false;
+    }
+
+  
   }
 
-}
+  
