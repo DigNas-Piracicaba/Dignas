7b15d32737babb810cf488a581282514c49f1b9d
Henrique <henriquepiovaniorcatti@gmail.com>
2021-12-20 18:00:40 -0300

    completando forms rural

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -11,14 +11,54 @@ export class FormularioComponent implements OnInit {
   constructor(
   ) { }
 
-  ngOnInit(): void {
-  }
+    isShown: boolean = false
+    isShown2: boolean = false
+    isShown3: boolean = false
+    isShown4: boolean = false
+    isShown5: boolean = false
 
-  isShown: boolean = false
+    ngOnInit(): void {
+    }
+  
+    toggleShowT1() {
+      this.isShown = true;
+    }
 
-  toggleShow() {
-    this.isShown = !this.isShown;
-  }
+    toggleShowF1() {
+      this.isShown = false;
+    }
+
+    toggleShowT2() {
+      this.isShown2 = true;
+    }
+
+    toggleShowF2() {
+      this.isShown2 = false;
+    }
+
+    toggleShowT3() {
+      this.isShown3 = true;
+    }
+
+    toggleShowF3() {
+      this.isShown3 = false;
+    }
+
+    toggleShowT4() {
+      this.isShown4 = true;
+    }
+
+    toggleShowF4() {
+      this.isShown4 = false;
+    }
+
+    toggleShowT5() {
+      this.isShown5 = true;
+    }
+
+    toggleShowF5() {
+      this.isShown5 = false;
+    }
 
 }
 
